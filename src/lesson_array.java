fe7cc4c4d45ff7ee87ebb47866501fd24344fdf2
powellNK <89803160948v@gmail.com>
2024-03-24 15:35:42 +0300

    first

--- src/lesson_array.java
+++ src/lesson_array.java
@@ -1,5 +1,35 @@
 public class lesson_array {
     public static void main(String[] args){
-        System.out.println("g");
+        int [] numbers = new int[5]; // массив из 5 элементов. по умолчанию нулевое значение для типа данных
+        // int 0
+        //String null
+        // long 0
+
+        for (int index = 0; index< numbers.length; index++){
+            numbers[index] = index*index;
+        }
+        for (int index = 0; index< numbers.length; index++){
+            System.out.println(numbers[index]);
+        }
+
+
+        int [] num = new int[20];
+        for (int index = 0; index<num.length; index++){
+            num[index] = index+1;
+            System.out.println(num[index]);
+        }
+        System.out.println(num.length);
+        for (int index = num.length-1; index>=0; index--){
+            System.out.println(num[index]);
+        }
+        System.out.println(num[num.length-1]);
+        System.out.println(num[num.length-2]);
+        int element = 4;
+        for (int index = 0; index<num.length; index++){
+            if (num[index] == element){
+                System.out.println(index);
+                break;
+            }
+        }
     }
 }
